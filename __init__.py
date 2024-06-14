8b6c7d2c9bf8dde0576af84372ca066e9c9b4ef1
Guilherme Alves Lopes <guilhermealveslopes370@gmail.com>
2024-06-14 14:15:07 -0300

    First Commit

---
+++ __init__.py
@@ -0,0 +1,88 @@
+from typing import NamedTuple
+from torch import Tensor
+import torch
+import matplotlib.pyplot as plt
+
+BACKBONE_ENUM = {
+    'text': 'context_block',
+    'latent': 'x_block'
+}
+
+def colormap_tensor(colormap, tensor: Tensor):
+    if (colormap == 'none'):
+        return tensor.unsqueeze(-1).repeat(1, 1, 3)
+
+    # Normalize the tensor to the range [0, 1]
+    tensor_min = tensor.min()
+    tensor_max = tensor.max()
+    tensor_normalized = (tensor - tensor_min) / (tensor_max - tensor_min)
+    tensor_np = tensor_normalized.numpy()
+
+    colormap = plt.get_cmap(colormap)
+    tensor_colored_np = colormap(tensor_np)
+    tensor_colored = torch.from_numpy(tensor_colored_np[:, :, :3])
+    return tensor_colored
+
+matplotlib_colormaps = plt.colormaps()
+
+class RenderAttentionSpot:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "sd3_model": ("MODEL",),
+                "joint_block": ("NUMBER", {"default": 0, "max": 23}),
+                "backbone": (["text", "latent"],),
+                "view": (["query", "key", "value", "all-stacked", "all-interposed"]),
+                "colormap": (["none", *matplotlib_colormaps],)
+            }
+        }
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "render"
+
+    CATEGORY = "SD3 Power Lab/Visualize"
+
+    def render(self, sd3_model, joint_block, backbone, view, colormap):
+        km = sd3_model.model_state_dict()
+
+        tensor_location = f'joint_blocks.{joint_block}.{BACKBONE_ENUM[backbone]}.attn.qkv.weight'
+        attention_tensor: Tensor = None
+
+        for k in km:
+            if tensor_location in k:
+                attention_tensor = km[k]
+        
+        if (attention_tensor is None):
+            raise f"Could not locate attention tensor {tensor_location}"
+        
+        pre_image_tensor: Tensor = None
+        q,k,v = None
+
+        if (view == 'all-stacked'):
+            pre_image_tensor: Tensor = attention_tensor
+        elif (view == 'all-interposed'):
+            pre_image_tensor=  attention_tensor.view(1536, 1536, 3)
+        else:
+            q,k,v = torch.split(attention_tensor, 1536)
+
+        if (view == 'query'):
+            pre_image_tensor = q
+        elif (view == 'key'):
+            pre_image_tensor = k
+        elif (view == 'value'):
+            pre_image_tensor = v
+
+        if len(pre_image_tensor.shape) == 3:
+            return (pre_image_tensor.unsqueeze(0), )
+        else:
+            return colormap_tensor(colormap, pre_image_tensor)
+
+
+NODE_CLASS_MAPPINGS = {
+    "G370SD3PowerLab_RenderAttention": RenderAttentionSpot
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "G370SD3PowerLab_RenderAttention": "Render SD3 Attention"
+}
